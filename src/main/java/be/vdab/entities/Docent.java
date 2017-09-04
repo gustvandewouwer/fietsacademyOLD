c3d8711aac726e3ab9b26324957b18a145445540
Gust Van de Wouwer <gust.vandewouwer@gmail.com>
2017-09-04 11:51:39 +0200

    commit blz 17

---
+++ src/main/java/be/vdab/entities/Docent.java
@@ -0,0 +1,67 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "docenten")
+public class Docent implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	private long id;
+	private String voornaam;
+	private String familienaam;
+	private BigDecimal wedde;
+	private long rijksRegisterNr;
+
+	// je maakt getters voor de private variabelen, behalve voor serialVersionUID
+
+	
+	public String getNaam() {
+		return voornaam + ' ' + familienaam;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+
+	public String getFamilienaam() {
+		return familienaam;
+	}
+
+	public void setFamilienaam(String familienaam) {
+		this.familienaam = familienaam;
+	}
+
+	public BigDecimal getWedde() {
+		return wedde;
+	}
+
+	public void setWedde(BigDecimal wedde) {
+		this.wedde = wedde;
+	}
+
+	public long getRijksRegisterNr() {
+		return rijksRegisterNr;
+	}
+
+	public void setRijksRegisterNr(long rijksRegisterNr) {
+		this.rijksRegisterNr = rijksRegisterNr;
+	}
+}
